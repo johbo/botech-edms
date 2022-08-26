de9630ecd50dcc3db9cbe9072d420805800529a3
Johannes Bornhold <johannes@bornhold.name>
2022-08-26 23:18:32 +0200

    Printing stub accounting date into the image

--- botech/edms/transformation_mixins.py
+++ botech/edms/transformation_mixins.py
@@ -3,6 +3,10 @@ import pkgutil
 
 from PIL import Image, ImageColor, ImageDraw, ImageFont
 
+
+ANCHOR_RIGHT_ASCENDER = 'ra'
+ANCHOR_RIGHT_DESCENDER = 'rd'
+
 class TransformationStampAccountingMetadataMixin:
 
     def stamp_accounting_data(self):
@@ -12,11 +16,31 @@ class TransformationStampAccountingMetadataMixin:
         font_file = io.BytesIO(font_bytes)
         font = ImageFont.truetype(font=font_file, size=80)
 
-        stamp_text = "BOOKED"
-
-        length = font.getlength(stamp_text)
-        pos_x = self.image.size[0] - length - 100
-
         draw = ImageDraw.Draw(im=self.image)
-        draw.text((pos_x, 100), stamp_text, font=font, fill=128)
+
+        # TODO: calculated positions better:
+        # - x percent from the right
+        # - x percent from the bottom
+        pos_x = self.image.size[0] - 200
+        pos_y = 100
+        stamp_text = self.acct_doc_number
+
+        draw.text(
+            (pos_x, pos_y), stamp_text,
+            anchor=ANCHOR_RIGHT_ASCENDER, font=font, fill=128)
+
+        pos_y = 200
+        stamp_text = self.acct_booked_stamp
+
+        draw.text(
+            (pos_x, pos_y), stamp_text,
+            anchor=ANCHOR_RIGHT_ASCENDER, font=font, fill=128)
+
+        pos_y = self.image.size[1] - 500
+        stamp_text = self.acct_assignment
+
+        draw.text(
+            (pos_x, pos_y), stamp_text,
+            anchor=ANCHOR_RIGHT_DESCENDER, font=font, fill=128)
+
         return self.image
