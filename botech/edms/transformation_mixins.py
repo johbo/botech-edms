decda5ebbf14fad8ebbbf5c3e08e18dfc05ff738
Johannes Bornhold <johannes@bornhold.name>
2022-08-26 04:02:04 +0200

    Stamp a simple "BOOKED" into the generated image

--- botech/edms/transformation_mixins.py
+++ botech/edms/transformation_mixins.py
@@ -1,6 +1,22 @@
+import io
+import pkgutil
+
+from PIL import Image, ImageColor, ImageDraw, ImageFont
 
 class TransformationStampAccountingMetadataMixin:
 
     def _execute_on(self, *args, **kwargs):
-        # TODO: continue here!
-        raise NotImplementedError()
+        # TODO: opacity? RGBA mode?
+
+        font_bytes = pkgutil.get_data('botech.edms', 'fonts/Roboto-Bold.ttf')
+        font_file = io.BytesIO(font_bytes)
+        font = ImageFont.truetype(font=font_file, size=80)
+
+        stamp_text = "BOOKED"
+
+        length = font.getlength(stamp_text)
+        pos_x = self.image.size[0] - length - 100
+
+        draw = ImageDraw.Draw(im=self.image)
+        draw.text((pos_x, 100), stamp_text, font=font, fill=128)
+        return self.image
